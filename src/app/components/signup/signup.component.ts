5cee1f3b80f4cf233d068098fff21cbc35888fe4
ravi@ttn.com <booler@ttn.com>
2019-06-23 09:42:29 +0530

    added signup component

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 
 declare var $: any;
 
@@ -10,81 +11,114 @@ declare var $: any;
 })
 export class SignupComponent implements OnInit {
 
-  constructor(private router: Router, private route: ActivatedRoute) {
+  signupForm: FormGroup;
+
+  constructor(private router: Router, private route: ActivatedRoute, private fb: FormBuilder) {
 
   }
 
   ngOnInit() {
+    const that = this;
+    this.signupForm = new FormGroup({
+      email: new FormControl(),
+      password: new FormControl(),
+      confirm: new FormControl(),
+      firstName: new FormControl(),
+      surname: new FormControl(),
+      contact: new FormControl(),
+      whatsapp: new FormControl(),
+      address: new FormControl(),
+      city: new FormControl(),
+      state: new FormControl(),
+      country: new FormControl(),
+      gender: new FormControl(),
+      college: new FormControl(),
+      course: new FormControl(),
+      year: new FormControl(),
+      achievements: new FormControl(),
+      hobbies: new FormControl(),
+      skills: new FormControl()
+    });
 
-      // $('#wizard').steps();
-      $('#form').steps({
-        bodyTag: 'fieldset',
-        onStepChanging: function (event, currentIndex, newIndex) {
-          // Always allow going backward even if the current step contains invalid fields!
-          if (currentIndex > newIndex) {
-            return true;
-          }
-
-          // Forbid suppressing "Warning" step if the user is to young
-          if (newIndex === 3 && Number($('#age').val()) < 18) {
-            return false;
-          }
-
-          // tslint:disable-next-line:prefer-const
-          let form = $(this);
-
-          // Clean up if user went backward before
-          if (currentIndex < newIndex) {
-            // To remove error styles
-            $('.body:eq(' + newIndex + ') label.error', form).remove();
-            $('.body:eq(' + newIndex + ') .error', form).removeClass('error');
-          }
-
-          // Disable validation on fields that are disabled or hidden.
-          form.validate().settings.ignore = ':disabled,:hidden';
-
-          // Start validation; Prevent going forward if false
-          return form.valid();
-        },
-        onStepChanged: function (event, currentIndex, priorIndex) {
-          // Suppress (skip) "Warning" step if the user is old enough.
-          if (currentIndex === 2 && Number($('#age').val()) >= 18) {
-            $(this).steps('next');
-          }
-
-          // Suppress (skip) "Warning" step if the user is old enough and wants to the previous step.
-          if (currentIndex === 2 && priorIndex === 3) {
-            $(this).steps('previous');
-          }
-        },
-        onFinishing: function (event, currentIndex) {
-          const form = $(this);
-
-          // Disable validation on fields that are disabled.
-          // At this point it's recommended to do an overall check (mean ignoring only disabled fields)
-          form.validate().settings.ignore = ':disabled';
-
-          // Start validation; Prevent form submission if false
-          return form.valid();
-        },
-        onFinished: function (event, currentIndex) {
-          const form = $(this);
-
-          // Submit form input
-          form.submit();
+    $('#form').steps({
+      bodyTag: 'fieldset',
+      onStepChanging: function (event, currentIndex, newIndex) {
+        // Always allow going backward even if the current step contains invalid fields!
+        if (currentIndex > newIndex) {
+          return true;
         }
-      }).validate({
-        errorPlacement: function (error, element) {
-          element.before(error);
-        },
-        rules: {
-          confirm: {
-            equalTo: '#password'
-          }
-        }
-      });
 
-    $('.chosen-select').chosen({width: "100%"});
+        // Forbid suppressing "Warning" step if the user is to young
+        if (newIndex === 3 && Number($('#age').val()) < 18) {
+          return false;
+        }
+
+        // tslint:disable-next-line:prefer-const
+        let form = $(this);
+
+        // Clean up if user went backward before
+        if (currentIndex < newIndex) {
+          // To remove error styles
+          $('.body:eq(' + newIndex + ') label.error', form).remove();
+          $('.body:eq(' + newIndex + ') .error', form).removeClass('error');
+        }
+
+        // Disable validation on fields that are disabled or hidden.
+        form.validate().settings.ignore = ':disabled,:hidden';
+
+        // Start validation; Prevent going forward if false
+        return form.valid();
+      },
+      onStepChanged: function (event, currentIndex, priorIndex) {
+        // Suppress (skip) "Warning" step if the user is old enough.
+        if (currentIndex === 2 && Number($('#age').val()) >= 18) {
+          $(this).steps('next');
+        }
+
+        // Suppress (skip) "Warning" step if the user is old enough and wants to the previous step.
+        if (currentIndex === 2 && priorIndex === 3) {
+          $(this).steps('previous');
+        }
+      },
+      onFinishing: function (event, currentIndex) {
+        const form = $(this);
+
+        // Disable validation on fields that are disabled.
+        // At this point it's recommended to do an overall check (mean ignoring only disabled fields)
+        form.validate().settings.ignore = ':disabled';
+
+        // Start validation; Prevent form submission if false
+        return form.valid();
+      },
+      onFinished: function (event, currentIndex) {
+        const form = $(this);
+
+        // // Submit form input
+        // form.submit();
+        that.submitForm();
+      }
+    }).validate({
+      errorPlacement: function (error, element) {
+        element.before(error);
+      },
+      rules: {
+        confirm: {
+          equalTo: '#password'
+        }
+      }
+    });
+
+    $('.chosen-select').chosen({width: '100%'});
   }
 
+  submitForm = () => {
+    if (this.signupForm.invalid) {
+      console.log('hello', this.signupForm.value);
+      this.signupForm.get('email').markAsTouched();
+      this.signupForm.get('password').markAsTouched();
+      return;
+    } else {
+      console.log(this.signupForm.value);
+    }
+  }
 }
