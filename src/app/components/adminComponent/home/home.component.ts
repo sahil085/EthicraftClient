96765f2e612e573912d27d9a2917a5c91624bf54
Ravi <ravi97garg@github.com>
2019-09-13 12:56:22 +0530

    vertically centered the login form

--- src/app/components/adminComponent/home/home.component.ts
+++ src/app/components/adminComponent/home/home.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  username: string;
+
+  constructor() {
+    console.log(JSON.parse(localStorage.getItem('user')));
+  }
 
   ngOnInit() {
   }
